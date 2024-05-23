28436b463840639e1ad0b7cf4affa1194dc65246
username <milan.szalai@ivm-micrologistics.com>
2024-05-23 14:50:46 +0200

    app skeleton

---
+++ src/main/java/com/overissue/LibraSphere/LibraSphereApplication.java
@@ -0,0 +1,12 @@
+package com.overissue.LibraSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraSphereApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(LibraSphereApplication.class, args);
+ }
+}
---
+++ src/test/java/com/overissue/LibraSphere/LibraSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overissue.LibraSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
